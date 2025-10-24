f87a31819435a993baef753296170988f6c6b709
UDAYVEERUS <udayveerus348566@gmail.com>
2025-10-24 22:17:23 +0530

    -

--- src/app/Shipping/page.tsx
+++ src/app/Shipping/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Shipping = () => {
   return (
-    <div>Shipping</div>
-  )
-}
- 
-export default Shipping
+    <div>
+      Shipping & Delivery Policy Last updated on Oct 24 2025 For International
+      buyers, orders are shipped and delivered through registered international
+      courier companies and/or International speed post only. For domestic
+      buyers, orders are shipped through registered domestic courier companies
+      and /or speed post only. Orders are shipped within 0-7 days or as per the
+      delivery date agreed at the time of order confirmation and delivering of
+      the shipment subject to Courier Company / post office norms. VIJENDRA
+      SINGH is not liable for any delay in delivery by the courier company /
+      postal authorities and only guarantees to hand over the consignment to the
+      courier company or postal authorities within 0-7 days rom the date of the
+      order and payment or as per the delivery date agreed at the time of order
+      confirmation. Delivery of all orders will be to the address provided by
+      the buyer. Delivery of our services will be confirmed on your mail ID as
+      specified during registration. For any issues in utilizing our services
+      you may contact our helpdesk on 9838824898 or olditemss@gmail.com
+    </div>
+  );
+};
+
+export default Shipping;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact Page</div>
+    <div>Contact us
+
+Last updated on Oct 24 2025
+
+You may contact us using the information below:
+
+Merchant Legal entity name: VIJENDRA SINGH
+Registered Address: Maharana Pratap Nagar Jhansi Uttar Pradesh Jhansi UTTAR PRADESH 284128
+Operational Address: Maharana Pratap Nagar Jhansi Uttar Pradesh Jhansi UTTAR PRADESH 284128
+Telephone No: 9838824898
+E-Mail ID: olditemss@gmail.com</div>
   )
 }
 
